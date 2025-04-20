e0a8cba6099f3f73c26b0b6e40eb99e72a3f44e6
hibaanadani <hibaanadani1@gmail.com>
2025-04-20 15:03:01 +0300

    created cred.jd and added eventlistners

---
+++ cred.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const formTitle = document.getElementById("form-title");
+    const authForm = document.getElementById("auth-form");
+    const emailField = document.getElementById("email");
+    const authButton = document.getElementById("auth-btn");
+    const toggleForm = document.getElementById("toggle-form");
+    
+    

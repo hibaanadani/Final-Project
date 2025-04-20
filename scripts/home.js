2b203d061e67fac7af3e991d653f198c2e5ec112
hibaanadani <hibaanadani1@gmail.com>
2025-04-20 17:24:07 +0300

    started home page script with user welcome message

---
+++ scripts/home.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const headerText = document.getElementById("header-txt");
+    const quizList = document.getElementById("quiz-list");
+
+    let user = localStorage.getItem("user");
+    if (user) {
+        headerText.textContent = `Welcome ${currentUser}! Please Select a quiz`;
+    }
+});

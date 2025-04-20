90c57b9fdbf6e1065dee29a22e3715d83f9cb793
hibaanadani <hibaanadani1@gmail.com>
2025-04-20 21:06:57 +0300

    initialized quiz.js file

---
+++ scripts/quiz.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const selectedQuiz = localStorage.getItem("selectedQuiz");
+    const questionContainer = document.getElementById("question-container");
+
+});

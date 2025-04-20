1e433b319c31a3c69761b14a962e77c382d0eab7
hibaanadani <hibaanadani1@gmail.com>
2025-04-20 15:33:31 +0300

    added the toggle functionality to switch from log in to register, and vise versa

--- cred.js
+++ cred.js
@@ -5,4 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const authButton = document.getElementById("auth-btn");
     const toggleForm = document.getElementById("toggle-form");
     
-    
+    let isRegister = false;
+
+    toggleForm.addEventListener("click", () => {
+        isRegister = !isRegister;
+        headerText.textContent = isRegister ? "Welcome! Please Register" : "Welcome! Please Log In";
+        authButton.textContent = isRegister ? "Register" : "Login";
+        emailField.parentElement.classList.toggle("hidden", isRegister);
+        toggleForm.innerHTML = isRegister
+            ? `Already have an account? <span>Login</span>`
+            : `Don't have an account? <span>Register</span>`;
+    });
+
+});
